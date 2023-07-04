3dea56d34387af99b586cb2b87aeeb78e2abff52
Prajakta Dhumal <124650559+Dhumalprajakta@users.noreply.github.com>
2023-07-04 18:09:13 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<style>
+		/* Add CSS styling for the page here */
+	</style>
+</head>
+<body>
+	<section class="company-info">
+		    <h1 style="text-align:center;color:blue;font-size:90;text-decoration:underline;"
+    >Online Examination System</h1>
+		 <p style="font-size:20px;"><span style="font-size:40px">T</span>he Online Examination System is a technology-driven way to simplify examination activities like defining exam patterns with question banks, defining exam timers, objective/ subjective question sections, and conducting exams using a computer or mobile devices in a paperless manner.
+
+Online Examination System is a cost-effective, scalable way to convert traditional pen and paper-based exams to online and paperless mode. Candidates can appear for the exam using any desktop, laptop, or mobile device with a browser. Exam results can be generated instantly for the objective type of questions.
+
+It can simplify overall examination management and result in generation activity.
+It is a useful tool for conducting assessments along with remote proctoring.
+
+You can conduct online video streaming or can capture photographs of the candidate after a certain time interval during the online exam process.
+
+Remote Proctoring helps to keep track of the overall activities of the candidate during the online exam process.  It is possible to conduct the assessment process using a tablet to manage the offline mode exam process.
+<br>It is an efficient and cost-effective way to conduct online exams in multiple languages.
+
+There is a facility to conduct subjective exams. In the subjective exams, the candidate can type an answer to the theory question. Theory answers can be easily evaluated by the examiner using the exam admin panel and marks can be assigned according to answers typed by the student.
+
+You can also define your own custom assessment.<br><br>What are the types of online examinations?<br>
+<br>The following types of Questions are Supported in the system<br>
+
+1.Multiple Choice Questions<br>
+2.Fill in the blanks<br>
+3.Match the Following<br>
+4.True/ False type questions<br>
+5.Multiple Selection of Correct Answers<br>
+6.Subjective Questions to write answers<br>
+7.Image-Based Questions<br>
+8.Audio/ Video-based questions<br>
+9.Psychometric Questions<br>
+<br>You can conduct various types of Exams using Online Examination System<br><br>
+
+I. Admission Test<br>
+II. Center based exam<br>
+III. Coding Test<br>
+IV. Online Oral Exam<br>
+V. Mock test<br>
+VI. Formative assessments<br>
+VII. CBT Exam<br>
+VIII. English Language Aptitude Test<br>
+IX.Mathematics Aptitude Test<br>
+X. Descriptive Writing Test<br>
+XI. Practice test<br>
+XII. Thinking Skill Assessment<br>
+XIII. Business Skill Assessment<br>
+XIV. Leadership Skill Assessment<br>
+XVI. Industrial Behavior Assessment<br>
+XVII.Out of the Box Thinking assessment<br>
+XVIII.Voice Assessment Test<br>
+XIX.Career Aptitude Test<br>
+XX.Psychometric Tests<br>
+
+
+
+
+
+</p>
+	
+
+
+
+
+	
+	</section>
+</body>
+</html>
